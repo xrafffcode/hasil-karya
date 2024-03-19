f15832d2958d371250676110411f015aaf1fac5b
xrafffcode <yusfiwawan2@gmail.com>
2024-03-20 02:36:51 +0700

    up

---
+++ deploy.js
@@ -0,0 +1,18 @@
+const execa = require("execa")
+const fs = require("fs");
+
+(async () => {
+  try {
+    await execa("npm", ["run", "build"])
+    console.log("Building...")
+
+    await execa("git", ["add", "dist"])
+    await execa("git", ["commit", "-m", "deploy"])
+    await execa("git", ["push", "origin", "main"])
+    console.log("Successfully deployed")
+  } catch (e) {
+    console.log(e.message)
+    process.exit(1)
+  }
+}
+)()
